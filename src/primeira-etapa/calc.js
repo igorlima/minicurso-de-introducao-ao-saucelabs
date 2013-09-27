da1a6744a115964b81c816adf91b2009c5542a8c
Igor Lima <lima.igorribeiro@gmail.com>
2013-09-27 15:11:18 -0300

    Add few code of examples

---
+++ src/primeira-etapa/calc.js
@@ -0,0 +1,5 @@
+(function (global) {
+  global.myCalc = {
+
+  };
+})(this);
---
+++ src/primeira-etapa/calc_spec.js
@@ -0,0 +1,5 @@
+describe("Minha calculadora", function() {
+  it("um exemplo de especificação", function() {
+    expect(true).toBe(true);
+  });
+});
