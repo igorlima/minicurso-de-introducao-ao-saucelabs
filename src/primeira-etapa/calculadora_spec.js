5ce6d5c4511c5cece9445c5ccf5eb1cd7b6591df
Igor Lima <lima.igorribeiro@gmail.com>
2013-09-27 16:55:28 -0300

    Add an example of specification

--- src/primeira-etapa/calculadora_spec.js
+++ src/primeira-etapa/calculadora_spec.js
@@ -1,5 +1,11 @@
 describe("Minha calculadora", function() {
+
+  it("um exemplo de especificação", function() {
+    expect(true).toBe(true);
+  });
+
   it("a soma 2 + 2 DEVE ser 4", function() {
     expect( Calculadora.soma( 2, 2 ) ).toBe( 4 );
   });
+
 });
